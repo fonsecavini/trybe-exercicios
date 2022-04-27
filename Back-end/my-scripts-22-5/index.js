b5fcc1eb720625ff30fafa3ffacdf295cd1c67ca
vinicius <vfonseca.eng@gmail.com>
2022-04-27 07:14:16 -0300

    HTTP com NodeJS continuação

--- Back-end/my-scripts-22-5/index.js
+++ Back-end/my-scripts-22-5/index.js
@@ -15,11 +15,6 @@ function handleHelloWorldRequest(req, res) {
 /* const express = require('express');
 const app = express();
 
-const recipes = [
-  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
-  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
-];
 
 app.get('/recipes', function (req, res) {
     res.json(recipes);
@@ -41,6 +36,16 @@ const drinks = [
     { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
+app.get('/drinks/:id', function (req, res) {
+    const { id } = req.params;
+    const drink = drinks.find((d) => d.id === parseInt(id));
+
+    if(!drink) return res.status(404).json({ message: 'Drink Not Found'});
+
+    res.status(200).json(drink);
+
+})
+
 app.get('/drinks', function (req, res) {
     res.json(drinks);
 });
@@ -48,3 +53,30 @@ app.get('/drinks', function (req, res) {
 app.listen(3001, () => {
     console.log('Aplicação rodando')
 });
+
+
+const recipes = [
+    { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+    { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+    { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+  ];
+
+  app.get('/recipes/search', function (req, res) {
+    const { name, maxPrice, minPrice } = req.query;
+    const filterRecipes = recipes.filter((r) => r.name.includes(name) && r.price > parseInt(minPrice) && r.price < parseInt(maxPrice));
+
+    res.status(200).json(filterRecipes);
+
+})
+
+
+app.get('/recipes/:id', function (req, res) {
+    const { id } = req.params;
+    const recipe = recipes.find((r) => r.id === parseInt(id));
+  
+    if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+  
+    res.status(200).json(recipe);
+  });
+
+  
